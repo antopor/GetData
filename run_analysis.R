21c8b0a955fab401711465baae0cf398c0b18f6e
antopor <antopor@users.noreply.github.com>
2015-10-14 00:57:31 +0300

    Reorder and rename columns

--- run_analysis.R
+++ run_analysis.R
@@ -15,22 +15,22 @@ data.y = fread(paste(dataset_dir, "/train/y_train.txt", sep = ""), sep=" ", head
 data.x = fread(paste(dataset_dir, "/train/X_train.txt", sep = ""), sep=" ", header = FALSE, colClasses = "numeric", stringsAsFactors= FALSE, select = feature.list.filtered);
 data.subject = fread(paste(dataset_dir, "/train/subject_train.txt", sep=""), header = FALSE, colClasses = "numeric", stringsAsFactors= FALSE);
 
-data.train = cbind(data.y, data.subject, data.x);
-names(data.train) = c("Label", "Person", feature.list$featureName[feature.list.filtered]);
+data.train = cbind(data.subject, data.y, data.x);
+names(data.train) = c("subject", "activity", feature.list$featureName[feature.list.filtered]);
 
 # load test data
 data.y = fread(paste(dataset_dir, "/test/y_test.txt", sep = ""), sep=" ", header = FALSE, colClasses = "numeric", stringsAsFactors= FALSE);
 data.x = fread(paste(dataset_dir, "/test/X_test.txt", sep = ""), sep=" ", header = FALSE, colClasses = "numeric", stringsAsFactors= FALSE, select = feature.list.filtered);
 data.subject = fread(paste(dataset_dir, "/test/subject_test.txt", sep=""), header = FALSE, colClasses = "numeric", stringsAsFactors= FALSE);
 
-data.test = cbind(data.y, data.subject, data.x);
-names(data.test) = c("Label", "Person", feature.list$featureName[feature.list.filtered]);
+data.test = cbind(data.subject, data.y, data.x);
+names(data.test) = c("subject", "activity", feature.list$featureName[feature.list.filtered]);
 
 # merge train and test data
-data = rbind(data.test, data.train) %>% group_by(Label, Person) %>% summarise_each(funs(mean));
+data = rbind(data.test, data.train) %>% group_by(subject, activity) %>% summarise_each(funs(mean));
 activity.labels = fread(paste(dataset_dir, "/activity_labels.txt", sep=""), sep=" ", col.names = c("id", "activity_name"), header = FALSE, colClasses = "character", stringsAsFactors = FALSE);
 
-data$Label = factor(data$Label, levels = activity.labels$id, labels = activity.labels$activity_name);
+data$Label = factor(data$activity, levels = activity.labels$id, labels = activity.labels$activity_name);
 rm(data.test, data.train, data.x, data.y, data.subject);
 
 # export data
